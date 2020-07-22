943e4ce851a33af9172580539304f0027338bc8d
Leo Chan <kele8929@163.com>
2020-07-22 16:22:53 +0800

    测试更新

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 
 const render = () => {
     ReactDOM.render(
-        <div>hello world</div>,
+        <div>hello world !</div>,
         document.querySelector('#app')
     )
 }
